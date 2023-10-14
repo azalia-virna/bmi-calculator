c70dcc12282474a00643b09048f13957e36fecf6
Azalia Virna <azaliavirna@gmail.com>
2023-10-14 15:21:45 +0700

    revisi js

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
-let button = document.getElementById('btn');
+window.onload = () => {
+  const btn = document.querySelector('#button');
+  btn.addEventListener('click', calcBMI)
+}
 
-button.addEventListener('click', () => {
-  const height = parseInt(document.getElementById('height').value);
-  const weight = parseInt(document.getElementById('weight').value);
-  const result = document.getElementById('output');
-  let height_status = false,
-    weight_status = false;
 
-  if (height === '' || isNaN(height) || (height <= 0)) {
-    document.getElementById('height_error').innerHTML = 'Please enter a valid height';
-  } else {
-    document.getElementById('height_error').innerHTML = ' ';
-    height_status = true;
-  }
-  
-  if (weight === '' || isNaN(weight) || (weight <= 0)) {
-    document.getElementById('weight_error').innerHTML = 'Please enter a valid weight';
-  } else {
-    document.getElementById('weight_error').innerHTML = ' ';
-    weight_status = true;
+function calcBMI() {
+  const height = document.querySelector('#height').value;
+  const weight = document.querySelector('#weight').value;
+  const result = document.querySelector('#result');
+
+  if (!height || isNaN(height) || height < 0) {
+      result.innerText = "Please provide a valid height";
+      return;
+  } else if (!weight || isNaN(weight) || weight < 0) {
+      result.innerText = "Please provide a valid weight";
+      return;
   }
 
-  if (height_status && weight_status) {
-    const bmi = (weight / ((height * height) / 1000)).toFixed(2);
+  const bmi = (weight / ((height * height) / 10000)).toFixed(2);
 
-    if (bmi < 18.6) {
-      result.innerHTML = 'Under Wight : ' + bmi;
-    } else if (bmi >= 18.6 && bmi < 24.9) {
-      result.innerHTML = 'Normal : ' + bmi;
-    } else {
-      result.innerHTML = 'Over Weight : ' + bmi;
-    }
+  if (bmi < 18.5) {
+      result.innerText = `You BMI Is ${bmi}
+       You Are Underweight`; 
+  } else if (bmi >= 18.5 && bmi < 24.9) {
+      result.innerText = `Your BMI Is ${bmi}
+      You Are Normal`;
+  } else if (bmi >= 25 && bmi < 29.9) {
+      result.innerText = `You BMI Is ${bmi}
+      You Are Overweight`;
+  } else if (bmi >= 30 && bmi < 34.9) {
+      result.innerText = `Your BMI Is ${bmi}
+      You Are Obesity`;
   } else {
-    alert('The form has errors');
-    result.innerHTML = '';
+      result.innerText = `Your BMI Is ${bmi}
+      You Are Extremely obese`;
   }
-});
+}
